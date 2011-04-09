1a07829ba636360987cf8569340eb5303b194bc4
alexeypetrushin <alexey.petrushin@gmail.com>
2011-04-09 09:21:31 +0400

    fixin some url stuff

---
+++ lib/services/mail.rb
@@ -0,0 +1,26 @@
+class Mail < ClusterManagement::Service
+  tag :basic
+  
+  def install
+    services.system_tools.install
+    
+    apply_once :install do |box|
+      logger.info "installing :#{service_name} to #{box}"
+      
+      # box.bash 'packager install sendmail', /Starting Mail Transport Agent (MTA) sendmail.*done/
+    end
+    self
+  end
+  
+  def start
+    self
+  end
+  
+  def stop
+    self
+  end
+    
+  def started?
+    true
+  end
+end
--- lib/services/rad_kit.rb
+++ lib/services/rad_kit.rb
@@ -3,7 +3,7 @@ class RadKit < ClusterManagement::Project
   
   project_options(
     requires: [
-      :basic,
+      :basic, :mail,
       :rad_core, :rad_ext, :rad_common_interface, :mongo_mapper_ext, :rad_saas
     ],
     name: 'rad_kit'
