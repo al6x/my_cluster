d67fd9730786710b4ed607ee9138333953af63af
alexeypetrushin <alexey.petrushin@gmail.com>
2011-06-26 06:06:00 +0400

    refactoring themes

--- lib/services/rad_abstract_interface.rb
+++
@@ -1,8 +0,0 @@
-class RadAbstractInterface < ClusterManagement::Project
-  tag 'app'
-  
-  project_options(
-    requires: [:basic, :rad_core],
-    name: 'rad_abstract_interface'
-  )
-end
---
+++ lib/services/rad_face.rb
@@ -0,0 +1,8 @@
+class RadFace < ClusterManagement::Project
+  tag 'app'
+  
+  project_options(
+    requires: [:basic, :rad_core],
+    name: 'rad_face'
+  )
+end
